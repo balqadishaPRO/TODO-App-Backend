d5831610fa68a80dc39e1b25cf6f2701fec65074
balqadishaPRO <aryngazievzasulan@gmail.com>
2025-01-06 20:53:57 +0500

    CORS

---
+++ src/main/java/dev/balqadisha/todo/WebConfig.java
@@ -0,0 +1,24 @@
+package dev.balqadisha.todo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://balqadishapro.github.io/TODO-App-Frontend/")  // Frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
