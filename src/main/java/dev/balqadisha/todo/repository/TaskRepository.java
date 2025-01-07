a0390e55e1e3650361612fbde6d03fece532b074
balqadishaPRO <aryngazievzasulan@gmail.com>
2025-01-07 21:54:49 +0500

    @Service

--- src/main/java/dev/balqadisha/todo/repository/TaskRepository.java
+++ src/main/java/dev/balqadisha/todo/repository/TaskRepository.java
@@ -2,6 +2,8 @@ package dev.balqadisha.todo.repository;
 
 import dev.balqadisha.todo.model.Task;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TaskRepository extends MongoRepository<Task, String> {
 }
