fbb532dc7b69f4f5909cce1cf0d2635dc7ec5895
balqadishaPRO <aryngazievzasulan@gmail.com>
2025-01-07 22:31:33 +0500

    temp

--- src/main/java/dev/balqadisha/todo/TodoApplication.java
+++ src/main/java/dev/balqadisha/todo/TodoApplication.java
@@ -2,10 +2,13 @@ package dev.balqadisha.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 @SpringBootApplication
 public class TodoApplication {
-
+	Dotenv dotenv = Dotenv.load();
+	String mongoUri = dotenv.get("MONGO_URI");
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
 	}
