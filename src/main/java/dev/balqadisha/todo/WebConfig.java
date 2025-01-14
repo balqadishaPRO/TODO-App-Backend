de6c3a51554643de6f08edd18b35ac00bb3ca95d
balqadishaPRO <aryngazievzasulan@gmail.com>
2025-01-14 18:10:32 +0500

    changed origin

--- src/main/java/dev/balqadisha/todo/WebConfig.java
+++ src/main/java/dev/balqadisha/todo/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://balqadishapro.github.io/TODO-Client") // Allow frontend origins
+                        .allowedOrigins("http://127.0.0.1:5500", "http://localhost:5500", "https://balqadishapro.github.io") // Allow frontend origins
                         .allowedMethods("GET", "POST", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*") // Allow all headers
                         .allowCredentials(true); // Allow cookies/credentials if needed
